c0ec9d6d4902f023f78af67be448d36a1cfdb4ab
Piyavadee Pariyavuth <piyavadeepariyavuth@gmail.com>
2018-03-04 11:21:58 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,11 +1,6 @@
 def oxford_comma(array)
-  if array.length == 2
+  if array.length == 1
     array.join(" and ")
-  elsif array.length == 1
-    array[0]
-  else
-    array[0..-2].join(", ") + ", and " + array[-1]
-
   end
 
 end
