2f4939c6be810d6d467511db0e4da0493069e100
Piyavadee Pariyavuth <piyavadeepariyavuth@gmail.com>
2018-03-04 11:38:35 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,7 +4,7 @@ def oxford_comma(array)
   elsif array.length == 2
     array.join(" and ")
   else
-    array[0..-1].join(", ") + array[-1] << (" and ")
+    array[0..-1].join(", ") + array[-2] << (" and ")
 
   end
 
